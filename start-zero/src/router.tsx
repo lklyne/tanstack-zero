82a23eab44f527f3d5f0f2dec40725c59bcd71dd
Austin Mudd <31991302+austinm911@users.noreply.github.com>
2025-03-09 22:40:15 -0700

    fix

--- start-zero/src/router.tsx
+++ start-zero/src/router.tsx
@@ -1,24 +1,27 @@
-import { createRouter as createTanstackRouter } from "@tanstack/react-router";
+import { createRouter as createTanstackRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from './routeTree.gen'
 
-import "./styles.css";
+import './styles.css'
 
 // Create a new router instance
 export const createRouter = () => {
 	const router = createTanstackRouter({
 		routeTree,
+		context: {
+			z: undefined,
+		},
 		scrollRestoration: true,
-	});
-	return router;
-};
+	})
+	return router
+}
 
-const router = createRouter();
+const router = createRouter()
 
 // Register the router instance for type safety
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
 	interface Register {
-		router: typeof router;
+		router: typeof router
 	}
 }
